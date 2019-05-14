e77810798253cc915d4646510288762124a0899f
dn07 <deepakmnahar@gmail.com>
2019-05-14 20:41:32 +0530

    imported

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.calculator;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.security.PrivateKey;
